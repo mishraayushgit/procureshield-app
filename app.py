6c84324b40c9b6400cb57c00fff268be2f46c138
mishraayushgit <156198404+mishraayushgit@users.noreply.github.com>
2026-06-03 22:51:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ from typing import Dict, List, Tuple
 
 import numpy as np
 import pandas as pd
-import plotly.express as px
+
 import plotly.graph_objects as go
-import streamlit as st
+
 from sklearn.ensemble import IsolationForest, RandomForestClassifier
 from sklearn.preprocessing import MinMaxScaler
 
